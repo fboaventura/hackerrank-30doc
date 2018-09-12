2ebeb3c57ce26623f30338b6c88e343427c808e4
Frederico Freire Boaventura <frederico@boaventura.net>
2018-09-12 11:02:45 -0300

    Day 0: Hello, World.

---
+++ day00/hello.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+"""
+Say Hello World plus a string line
+"""
+
+def main():
+    string = input()
+    if string:
+        print('Hello, World.')
+        print(string)
+
+
+# Main body
+if __name__ == '__main__':
+    main()
---
+++ day00/hello.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+read inputString
+
+echo "Hello, World."
+echo ${inputString}
