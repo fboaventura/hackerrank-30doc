30460da188fa92c12445d45cc06e6d91ce0d2bb3
Frederico Freire Boaventura <frederico@boaventura.net>
2018-09-12 11:03:39 -0300

    Day 1: Data Types

---
+++ day01/data_types.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+"""
+Declare, read and sum different data types variables
+"""
+
+
+def main():
+    i = 4
+    d = 4.0
+    s = 'HackerRank '
+
+    j = int(input())
+    e = float(input())
+    t = str(input())
+
+    print(i + j)
+    print(d + e)
+    print(s + t)
+
+# Main body
+if __name__ == '__main__':
+    main()
+
